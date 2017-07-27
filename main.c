a971ab0fbf41869f771f86c74c56132e56b1d5ac
Patrick Chin <p.ch@live.co.uk>
2017-07-27 04:27:58 +0100

    fixed one second lag

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+// vim: ts=8
 
 #include <stdio.h>
 #include <time.h>
@@ -18,13 +19,11 @@ static char const ab_month_name[][4] =
  * Print the current time in the default `date' format to the buffer buf of a
  * given size.
  */
-void snprintdate(char * buf, int size)
+void snprinttimer(char * buf, int size, time_t * timer)
 {
-    time_t timer;
-    time(&timer);
-    struct tm *t = localtime(&timer);
+    struct tm *t = localtime(timer);
     snprintf(buf, size,
-	    "%s %d %s %02d:%02d:%02d %s %d",
+	    "%s %d %s %02d:%02d:%02d %s %d\n",
 	    ab_weekday_name[t->tm_wday],	/* Day of week.  [0-6] */
 	    t->tm_mday,				/* Day.    [1-31] */
 	    ab_month_name[t->tm_mon],		/* Month.  [0-11] */
@@ -43,20 +42,28 @@ int main()
     struct timespec tp, rqtp = {0, 0};
     char buf[DATE_BUF_SIZE] = { '\0' };
 
-    snprintdate(buf, DATE_BUF_SIZE);
+    time_t timer;
+    time(&timer);
+
+    snprinttimer(buf, DATE_BUF_SIZE, &timer);
     fputs(buf, stdout);
     fflush(stdout);
 
     for (;;)
     {
+	// should the timer be updated every minute? testing needed
+	if ((long) timer % 60 == 0)
+	    time(&timer);
+	timer++; // get the date string for the next second
+	snprinttimer(buf, DATE_BUF_SIZE, &timer);
+
 	clock_gettime(CLOCK_MONOTONIC, &tp);
-	rqtp.tv_nsec = 1000000000l - tp.tv_nsec;
+	rqtp.tv_nsec = (1000000000-1) - tp.tv_nsec;
 	nanosleep(&rqtp, NULL);
 
+	// print the date as soon as sleep finished
 	fputs(buf, stdout);
 	fflush(stdout);
-
-	snprintdate(buf, DATE_BUF_SIZE);
     }
 
     return 0;
