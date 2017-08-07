e705551b4bcf9cbe0d3d84d7e2b19215b98a3cac
Patrick Chin <p.ch@live.co.uk>
2017-08-07 00:41:00 +0000

    added command line argument for time format

--- date-persist.c
+++ date-persist.c
@@ -4,23 +4,25 @@
 
 #define DATE_BUF_SIZE 100
 
-int main()
+int main(int argc, char *argv[])
 {
     time_t timer;
-    struct timespec tp, rqtp = {0, 0};
+    struct timespec tp;
     char * buf = malloc(DATE_BUF_SIZE);
+    const char * format = argc == 2 ? argv[1] : "%c\n";
 
     for (;;)
     {
 	time(&timer);
 	timer++; // get the date string for the next second
-	ctime_r(&timer, buf);
+	size_t len = strftime(buf, DATE_BUF_SIZE, format, localtime(&timer));
 
 	clock_gettime(CLOCK_MONOTONIC, &tp);
-	rqtp.tv_nsec = (1000000000-1) - tp.tv_nsec;
-	nanosleep(&rqtp, &rqtp);
+	tp.tv_sec = 0;
+	tp.tv_nsec = (1000000000-1) - tp.tv_nsec;
+	nanosleep(&tp, NULL);
 
-	fputs(buf, stdout);
+	fwrite(buf, 1, len, stdout);
 	fflush(stdout);
     }
 
