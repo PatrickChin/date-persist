847af5c605d5fc3832cc67a280114b1c88ef4c1a
Patrick Chin <p.ch@live.co.uk>
2017-11-16 02:30:27 +0000

    add newline to end buffer, not the format string

--- date-persist.c
+++ date-persist.c
@@ -1,41 +1,38 @@
 // vim: ts=8:sw=4:noexpandtab
 
-#include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-#include <string.h>
+
+#define BUF_SIZE 100
 
 int main(int argc, char *argv[])
 {
-    struct timespec curtp, sleeptp = {0, 0};
-    char buf[100];
-    char * format;
+    char * format = argc == 1 ? "%c\n" : argv[1];
 
-    if (argc != 2) {
-	format = "%c\n";
-    } else {
-	format = argv[1];
-	size_t flen = strlen(format);
-	if (format[flen-1] != '\n' &&
-		(format[flen-2] != '%' || format[flen-1] != 'n')) {
-	    format = malloc(flen+2);
-	    strncpy(format, argv[1], flen);
-	    format[flen] = '\n';
-	    format[flen+1] = '\0';
-	}
-    }
-
+    struct timespec curtp;
     clock_gettime(CLOCK_REALTIME, &curtp);
 
-    for (;;)
+    char buf[BUF_SIZE] = { '\0' };
+    struct timespec sleeptp = {0, 0};
+    while (1)
     {
 	long nextsec =  curtp.tv_sec + 1; // get the date string for the next second
 	struct tm * localtm = localtime(&nextsec);
 	size_t len = strftime(buf, sizeof buf, format, localtm);
 
-	// MONOTONIC_RAW and REALTIME seconds may not align
-	// REALTIME is affected by NTP
-	// TODO Find out what happens if realtime jumps backwards
+	// append newline
+	{
+	    char * c = buf;
+	    while ((c-buf) < BUF_SIZE && *c != '\0')
+		c++;
+	    if (*(c-1) != '\n')
+	    {
+		*c = '\n';
+		*++c = '\0';
+		len++;
+	    }
+	}
+
 	clock_gettime(CLOCK_REALTIME, &curtp);
 	sleeptp.tv_nsec = (1000000000-1) - curtp.tv_nsec;
 	nanosleep(&sleeptp, NULL);
